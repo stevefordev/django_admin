7ec90e4e0f355329dd04d6d1a273fef6d838b66b
steve park <32486591+stevefordev@users.noreply.github.com>
2024-05-17 17:05:16 +0900

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
+# myapp/models.py
 from django.db import models
 
-# Create your models here.
+class Alert(models.Model):
+    title = models.CharField(max_length=200)
+    status = models.CharField(max_length=50)
+    received_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+#python manage.py makemigrations
+#python manage.py migrate
