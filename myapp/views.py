cc3799591baf3a7ffb2c029a8fa62ecb1d2049e6
steve park <32486591+stevefordev@users.noreply.github.com>
2024-05-17 16:52:59 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# myapp/views.py
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Alert
+import json
 
-# Create your views here.
+@csrf_exempt
+def receive_alert(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        title = data.get('title')
+        status = data.get('status')
+        
+        # Alert 객체 생성 및 저장
+        Alert.objects.create(title=title, status=status)
+        
+        return JsonResponse({"message": "Alert received successfully"})
+    else:
+        return JsonResponse({"error": "Invalid request"}, status=400)
