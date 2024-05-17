5bd6df34157e67f0a03831d76c31f3e0cb5ae3fd
steve park <32486591+stevefordev@users.noreply.github.com>
2024-05-17 17:08:11 +0900

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# myproject/urls.py
 from django.contrib import admin
 from django.urls import path
+from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('webhook/', views.receive_alert),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
