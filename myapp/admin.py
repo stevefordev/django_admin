afb26ef6ccc1d5681e3c153aae5c1bfa33c962bb
stevefordev <stevefordev@gmail.com>
2024-05-17 07:33:17 +0000

    first

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,13 +1,3 @@
-# myapp/models.py
-from django.db import models
+from django.contrib import admin
 
-class Alert(models.Model):
-    title = models.CharField(max_length=200)
-    status = models.CharField(max_length=50)
-    received_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.title
-
-#python manage.py makemigrations
-#python manage.py migrate
+# Register your models here.
--- myapp/views.py
+++ myapp/views.py
@@ -1,19 +1,3 @@
-# myapp/views.py
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import Alert
-import json
+from django.shortcuts import render
 
-@csrf_exempt
-def receive_alert(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        title = data.get('title')
-        status = data.get('status')
-        
-        # Alert 객체 생성 및 저장
-        Alert.objects.create(title=title, status=status)
-        
-        return JsonResponse({"message": "Alert received successfully"})
-    else:
-        return JsonResponse({"error": "Invalid request"}, status=400)
+# Create your views here.
